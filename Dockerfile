ef74500bca8f215dcffaea13b61175460331ba96
singh Aman <amanssm45@gmail.com>
2025-05-02 12:25:32 +0530

    This is First Commit By Aman singh

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM openjdk:17
+
+COPY target/DockerDemo.jar  /usr/app/
+
+WORKDIR /usr/app/
+
+EXPOSE 8080
+
+ENTRYPOINT ["java" , "-jar" , "DockerDemo.jar"]
+
+
+
+
+
+
+# <finalName>DockerDemo</finalName> 
+# server.port=8080
+
+
---
+++ src/main/java/com/Docker/demo/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.Docker.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Hello EveryOne";
+	}
+	
+
+}
